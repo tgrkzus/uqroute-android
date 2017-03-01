cba4bf7059cad52a0ecc5a5d5db850910a1dfbbf
tgrkzus <tgrkzus@gmail.com>
2017-03-01 16:34:57 +1000

    Added some WIP search bar stuff, also made map zoom in on St Lucia by default

--- app/src/main/java/com/uqroute/uqroute/MainActivity.java
+++ app/src/main/java/com/uqroute/uqroute/MainActivity.java
@@ -11,12 +11,14 @@ import android.view.Window;
 import com.mapzen.android.graphics.MapFragment;
 import com.mapzen.android.graphics.MapzenMap;
 import com.mapzen.android.graphics.OnMapReadyCallback;
+import com.mapzen.tangram.LngLat;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private MapzenMap map;
     private boolean enableLocationOnResume = false;
+    private boolean queryingPermissions = false;
     static final int FINE_PERMISSION = 0;
 
     @Override
@@ -33,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             public void onMapReady(MapzenMap map) {
                 MainActivity.this.map = map;
+                map.setPosition(new LngLat(153.0147179, -27.4977877));
+                map.setRotation(0f);
+                map.setZoom(15f);
+                map.setTilt(0f);
 
                 if (getLocationPermissions()) {
                     map.setMyLocationEnabled(true);
@@ -68,9 +74,12 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         else {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    FINE_PERMISSION);
+            if (!queryingPermissions) {
+                queryingPermissions = true;
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        FINE_PERMISSION);
+            }
             return false;
         }
     }
@@ -79,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
                                            String permissions[], int[] grantResults) {
         switch (requestCode) {
             case FINE_PERMISSION: {
+                queryingPermissions = false;
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
