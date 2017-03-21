f9b470cfbfbe26c7b50cf1cc39c2254a83cb768e
Tom Richardson <tgrkzus@gmail.com>
2017-03-21 19:04:42 +1000

    Simple navigation drawer implementation for the location list activity

--- app/src/main/java/com/uqroute/uqroute/LocationListActivity.java
+++ app/src/main/java/com/uqroute/uqroute/LocationListActivity.java
@@ -2,18 +2,24 @@ package com.uqroute.uqroute;
 
 // Android Imports
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Color;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-        import android.support.v7.widget.RecyclerView;
-        import android.view.Menu;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.PopupMenu;
 
 // Mapzen Imports
@@ -46,6 +52,16 @@ class Location {
 
 public class LocationListActivity extends AppCompatActivity {
     private RecyclerView rv;
+
+    private ActionBarDrawerToggle drawerToggle;
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
+    static private String[] drawerOptions = {
+            "Your Timetable",
+            "Your Favourites",
+            "Settings"
+    };
+
     private List<Location> locations;
     private Comparator<Location> sortType;
 
@@ -122,22 +138,42 @@ public class LocationListActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_location_list);
-        setSupportActionBar((Toolbar) findViewById(R.id.location_toolbar));
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
-        getSupportActionBar().setIcon(R.mipmap.launcher_icon);
-
         if (BuildConfig.DEBUG) {
             Log.d(TAG, "Location list initialized");
         }
 
-        sortType = sortByBuildingNumber;
-        refreshLocationList();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_list);
 
-        // Setup
+        // Setup action bar
+        setSupportActionBar((Toolbar) findViewById(R.id.location_toolbar));
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        //getSupportActionBar().setDisplayUseLogoEnabled(true);
+        //getSupportActionBar().setIcon(R.mipmap.launcher_icon);
+
+        // Setup drawer
+        setupLocationDrawer();
+
+        // Setup location list
+        sortType = sortByBuildingNumber;
+        initLocationList();
+
         //rv.setOnTouchListener(itemClickListener);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
@@ -149,12 +185,20 @@ public class LocationListActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "Options item selected");
+        }
+
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            if (drawerLayout.isDrawerOpen(drawerList)) {
+                drawerLayout.closeDrawer(drawerList);
+            }
+            else {
+                drawerLayout.openDrawer(drawerList);
+            }
+            return true;
+        }
         switch(item.getItemId()) {
-            case R.id.action_settings:
-                if (BuildConfig.DEBUG) {
-                    Log.d(TAG, "Settings activated");
-                }
-                break;
             case R.id.action_sorting_options:
                 if (BuildConfig.DEBUG) {
                     Log.d(TAG, "Sorting menu activated");
@@ -180,7 +224,49 @@ public class LocationListActivity extends AppCompatActivity {
         finish();
     }
 
-    private void refreshLocationList() {
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem((int) id);
+        }
+    }
+    private void selectItem(int id) {
+        drawerLayout.closeDrawer(drawerList);
+    }
+
+    private void setupLocationDrawer() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.activity_location_list);
+        drawerList = (ListView) findViewById(R.id.location_drawer_list);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+            public void onDrawerOpened(View v) {
+                super.onDrawerOpened(v);
+
+                if (BuildConfig.DEBUG) {
+                    Log.d(TAG, "Drawer opened");
+                }
+            }
+
+            public void onDrawerClosed(View v) {
+                super.onDrawerClosed(v);
+
+                if (BuildConfig.DEBUG) {
+                    Log.d(TAG, "Drawer closed");
+                }
+            }
+        };
+
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.addDrawerListener(drawerToggle);
+
+        drawerList.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, drawerOptions));
+
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+    }
+
+    private void initLocationList() {
         // Setup list
         rv = (RecyclerView) this.findViewById(R.id.location_list_view);
 
@@ -189,20 +275,26 @@ public class LocationListActivity extends AppCompatActivity {
             locations = fetchJson();
         }
 
-        // Sort list of locations
-        Collections.sort(locations, sortType);
-        LocationListAdapter adapter = new LocationListAdapter(locations);
-
         // Linear layout
         LinearLayoutManager layout = new LinearLayoutManager(this);
 
         // Decor
         DividerItemDecoration divider = new DividerItemDecoration(rv.getContext(),
                 layout.getOrientation());
+
+
         rv.addItemDecoration(divider);
+        rv.setLayoutManager(layout);
+
+        refreshLocationList();
+    }
+
+    private void refreshLocationList() {
+        // Sort list of locations
+        Collections.sort(locations, sortType);
+        LocationListAdapter adapter = new LocationListAdapter(locations);
 
         rv.setAdapter(adapter);
-        rv.setLayoutManager(layout);
     }
 
     private ArrayList<Location> fetchJson() {
